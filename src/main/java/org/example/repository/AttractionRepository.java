8e50bc527c733c99c062ee19106783b346b944ea
AyalaVirtual <erica.ayala.work@gmail.com>
2024-05-07 18:46:43 -0400

    Create AttractionRepository interface and add method signatures.

---
+++ src/main/java/org/example/repository/AttractionRepository.java
@@ -0,0 +1,16 @@
+package org.example.repository;
+
+import org.example.model.Attraction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface AttractionRepository extends JpaRepository<Attraction, Long> {
+    // This method finds an attraction by its id
+    Optional<Attraction> findById(Long attractionId);
+    // This method finds an attraction by its name
+    Optional<Attraction> findByName(String attractionName);
+}
