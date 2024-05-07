47f178cfc531a0f115b7256116f81525c687f2a3
AyalaVirtual <erica.ayala.work@gmail.com>
2024-05-07 18:58:26 -0400

    Create ParkAccessibilityRepository interface and add method signatures.

---
+++ src/main/java/org/example/repository/ParkAccessibilityRepository.java
@@ -0,0 +1,14 @@
+package org.example.repository;
+
+import org.example.model.ParkAccessibility;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+
+@Repository
+public interface ParkAccessibilityRepository extends JpaRepository<ParkAccessibility, Long> {
+    // This method finds a park's accessibility details by id
+    Optional<ParkAccessibility> findById(Long parkAccessibilityId);
+
+}
