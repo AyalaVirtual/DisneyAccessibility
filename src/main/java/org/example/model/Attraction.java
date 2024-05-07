9b9d3b316ec88700426bdd58c1108228b8910ec5
AyalaVirtual <erica.ayala.work@gmail.com>
2024-05-06 22:47:08 -0400

    Correct error with Attraction model's attributes written in ParkAccessibility model. Update ERD image in README.

--- src/main/java/org/example/model/Attraction.java
+++ src/main/java/org/example/model/Attraction.java
@@ -1,5 +1,6 @@
 package org.example.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 
@@ -7,6 +8,116 @@ import jakarta.persistence.*;
 @Table(name="attractions")
 public class Attraction {
 
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column
+    private String name;
+
+    @Column
+    private String imageUrl;
+
+    @Column
+    private String description;
+
+    @Column
+    private double latitude;
+
+    @Column
+    private double longitude;
+
+    // This links the table representing the Attraction model to the table representing the Park model
+    @JsonIgnore
+    // This prevents a stack overflow/API crashing from parks and attractions calling each other back and forth
+    @ManyToOne // This means there is a one-to-one relationship between Attraction and Park
+    @JoinColumn(name = "park_id") // This represents the foreign key in SQL joining the columns to connect the 2 tables
+    private Park park;
+
+
+    public Attraction() {
+    }
+
+    public Attraction(Long id, String name, String imageUrl, String description, double latitude, double longitude, Park park) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.park = park;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Park getPark() {
+        return park;
+    }
+
+    public void setPark(Park park) {
+        this.park = park;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Attraction{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", description='" + description + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", park=" + park +
+                '}';
+    }
 
 }
--- src/main/java/org/example/model/ParkAccessibility.java
+++ src/main/java/org/example/model/ParkAccessibility.java
@@ -14,19 +14,7 @@ public class ParkAccessibility {
     private Long id;
 
     @Column
-    private String name;
-
-    @Column
-    private String imageUrl;
-
-    @Column
-    private String description;
-
-    @Column
-    private double latitude;
-
-    @Column
-    private double longitude;
+    private
 
     // This links the table representing the ParkAccessibility model to the table representing the Park model
     @JsonIgnore // This prevents a stack overflow/API crashing from parks and parkAccessibility calling each other back and forth
@@ -38,84 +26,6 @@ public class ParkAccessibility {
     public ParkAccessibility() {
     }
 
-    public ParkAccessibility(Long id, String name, String imageUrl, String description, double latitude, double longitude, Park park) {
-        this.id = id;
-        this.name = name;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.park = park;
-    }
 
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public Park getPark() {
-        return park;
-    }
-
-    public void setPark(Park park) {
-        this.park = park;
-    }
-
-
-    @Override
-    public String toString() {
-        return "ParkAccessibility{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", imageUrl='" + imageUrl + '\'' +
-                ", description='" + description + '\'' +
-                ", latitude=" + latitude +
-                ", longitude=" + longitude +
-                ", park=" + park +
-                '}';
-    }
 }
